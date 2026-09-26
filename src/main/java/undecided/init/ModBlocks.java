08921685c0ce950de780bff7e1299465ca877eaf
a6tumn <shinigami7x@proton.me>
2026-09-26 11:34:19 -0400

    Copied Prismarine block properties for Hydrolith blocks.

--- src/main/java/undecided/init/ModBlocks.java
+++ src/main/java/undecided/init/ModBlocks.java
@@ -5,9 +5,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import undecided.Undecided;
@@ -18,23 +17,9 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Undecided.MODID);
 
-    public static final DeferredBlock<Block> HYDROLITH_BRICKS = registerWithItem(
-            "hydrolith_bricks",
-            Block::new,
-            () -> BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(1.5F, 6.0F)
-    );
-
-    public static final DeferredBlock<Block> HYDROLITH_MOSAIC = registerWithItem(
-            "hydrolith_mosaic",
-            Block::new,
-            () -> BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(1.5F, 6.0F)
-    );
-
-    public static final DeferredBlock<Block> CHISELED_HYDROLITH_BRICKS = registerWithItem(
-            "chiseled_hydrolith_bricks",
-            Block::new,
-            () -> BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(1.5F, 6.0F)
-    );
+    public static final DeferredBlock<Block> HYDROLITH_BRICKS = registerWithItem("hydrolith_bricks", Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE));
+    public static final DeferredBlock<Block> HYDROLITH_MOSAIC = registerWithItem("hydrolith_mosaic", Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE));
+    public static final DeferredBlock<Block> CHISELED_HYDROLITH_BRICKS = registerWithItem("chiseled_hydrolith_bricks", Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE));
 
     public static <T extends Block> DeferredBlock<T> register(String name, Function<BlockBehaviour.Properties, T> block, Supplier<BlockBehaviour.Properties> properties) {
         return BLOCKS.register(name, () -> block.apply(properties.get().setId(ResourceKey.create(Registries.BLOCK, Undecided.id(name)))));
